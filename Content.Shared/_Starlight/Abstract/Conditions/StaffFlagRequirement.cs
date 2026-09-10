6f7b7d7b56dd4f26ada2f369a5bc593a041a7aff
Semper <rubinflare@gmail.com>
2026-09-09 22:28:43 -0400

    part one: make flags
    Adds two flags and in theory two conditions that use the flags.
    Unable to test properly in dev environment, haven't had the chance to test the requirements

---
+++ Content.Shared/_Starlight/Abstract/Conditions/StaffFlagRequirement.cs
@@ -0,0 +1,20 @@
+using Robust.Shared.Network;
+using Robust.Shared.Player;
+using Content.Shared.Administration;
+
+namespace Content.Shared._Starlight.Abstract.Conditions;
+
+public sealed partial class StaffFlagRequirement : BaseRequirement
+{
+    [DataField(required: true)]
+    public NetUserId UserId;
+
+    public override bool Handle(ICommonSession user)
+    {
+        base.Handle(user);
+        var flags = AdminFlags.None;
+        var isMentor = flags.HasFlag(AdminFlags.RNSLStaff);
+
+        return isMentor;
+    }
+}
